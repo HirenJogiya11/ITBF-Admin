25a3d139d50d4fbd0f4d55d696a93e94ff988469
HirenJogiya11 <lanetteam.hirenj@gmail.com>
2017-09-13 20:59:13 +0530

    introduction & userform change

--- src/app/introduction/introduction.component.ts
+++ src/app/introduction/introduction.component.ts
@@ -6,10 +6,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./introduction.component.css']
 })
 export class IntroductionComponent implements OnInit {
-
+    result:any;
+    imageName:string = null;
+    res:any;
+    final: string;
+    audioName:string = null;
   constructor() { }
 
   ngOnInit() {
   }
+    onChange($event):void {
 
+
+        this.readThis($event.target);
+    }
+
+    readThis(inputValue:any):void {
+        let file:File = inputValue.files[0];
+        let myReader:FileReader = new FileReader();
+
+        myReader.onloadend = (e) => {
+            this.result = myReader.result;
+            // Base64 data console.log(this.result);
+            this.imageName = file.name;
+            // image Name  console.log(this.imageName);
+        };
+        myReader.readAsDataURL(file);
+    }
+    onChangemp($event):void {
+    this.res = event.srcElement;
+    this.final = this.res.files[0].name;
+    }
+
+    read(inputValue:any):void {
+        let filedata:File = inputValue.files[0];
+        let myReader:FileReader = new FileReader();
+
+        myReader.onloadend = (e) => {
+            this.res = myReader.result;
+            // Base64 data console.log(this.result);
+            this.audioName = filedata.name;
+            // image Name  console.log(this.imageName);
+        };
+        myReader.readAsDataURL(filedata);
+    }
 }
