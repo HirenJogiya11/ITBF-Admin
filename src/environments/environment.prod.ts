9d856fd4ea63399be0b5d732b0b1226976f26a6a
HirenJogiya11 <lanetteam.hirenj@gmail.com>
2017-09-27 16:06:55 +0530

    adding prod confid

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+    production: true,
+    baseURL: 'http://34.210.231.178:4200',
+    login: '/login',
+    language: '/api/language',
+    pack: '/api/pack',
+    addIntroduction: '/addIntroductionData',
+    getIntroduction: '/getIntroductionData',
+    bulkUpload: '/api/user/bulkUpload',
+    userAdmin: '/api/user',
+    site: '/api/sites'
+  
 };
